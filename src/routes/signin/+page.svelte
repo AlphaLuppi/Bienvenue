e38fde6d98e3d3c5237a726e3f7bd467f7353ebe
Nicolas bataille <63408806+NicolasBataille@users.noreply.github.com>
2025-03-31 22:05:23 +0200

    Fixed page in signin path

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,25 +1,3 @@
-<script>
-	import { supabase } from '$lib/supabaseClient.js';
-	import { goto } from '$app/navigation';
-
-	function navigateToProvider(provider) {
-		const url = new URL('/signin/confirmOAuth', window.location.origin);
-		url.searchParams.set('provider', provider);
-		goto(url.toString());
-	}
-
-	const signInWithGoogle = async () => {
-		const { data, error } = await supabase.auth.signInWithOAuth({
-		provider: 'google',
-		options: {
-			redirectTo: `${window.location.origin}/auth/callback`,
-		},
-		});
-		
-		if (error) console.error('Erreur lors de la connexion avec Google:', error.message);
-	};
-</script>
-
 <form method="POST" action="?/login">
 	<label>
 		Email
@@ -34,5 +12,3 @@
 
 	<button formaction="?/oauth" value="google" name="provider">Sign in with Google</button>
 </form>
-
-<button on:click={() => navigateToProvider('google')}> Se connecter avec Google </button>
