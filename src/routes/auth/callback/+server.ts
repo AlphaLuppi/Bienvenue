7475b62b396c5adde7dddcaa4a21f4671cf3f5cd
Nicolas bataille <63408806+NicolasBataille@users.noreply.github.com>
2025-04-03 22:34:16 +0200

    Fixed missing auth scripts and removed unused auth pages

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -1,23 +1,23 @@
 import { redirect } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
 
-export const GET = async (event) => {
+export const GET: RequestHandler = async (event) => {
 	const {
 		url,
 		locals: { supabase }
 	} = event;
+
 	const code = url.searchParams.get('code') as string;
 	const next = url.searchParams.get('next') ?? '/';
-	const provider = url.searchParams.get('provider') as string;
 
-	//If we come back here from an OAuth page
 	if (code) {
 		const { error } = await supabase.auth.exchangeCodeForSession(code);
 		if (!error) {
 			throw redirect(303, `/${next.slice(1)}`);
-		} else {
-			console.log('autre');
-			console.log(error);
-			throw redirect(503, '/signin/error');
 		}
+		console.log(error);
 	}
+
+	// return the user to an error page with instructions
+	throw redirect(303, '/signin/error');
 };
